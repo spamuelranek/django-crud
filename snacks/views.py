086f07dec0bc5839dde9146220de1a9f739698b9
Samuel Panek <samuelrpanek@gmail.com>
2022-01-07 21:33:33 -0800

    adds CRUD for snack

---
+++ snacks/views.py
@@ -0,0 +1,29 @@
+from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
+from snacks.models import Snack
+from django.urls import reverse_lazy
+
+class HomePageView(TemplateView):
+  template_name = "home.html"
+
+class SnackListView(ListView):
+  template_name = "snack_list.html"
+  model = Snack 
+
+class SnackDetailView(DetailView):
+  teplate_name = "snack_detail.html"
+  model = Snack
+
+class SnackCreatView(CreateView):
+  template_name = "snack_create.html"
+  model = Snack
+  fields = ['title','purchaser','description']
+
+class SnackUpdateView(UpdateView):
+  template_name = "snack_update.html"
+  model = Snack
+  fields = ['title', 'description']
+
+class SnackDeleteView(DeleteView):
+  template_name = "snack_delete.html"
+  model = Snack
+  success_url = reverse_lazy("snack_list")
